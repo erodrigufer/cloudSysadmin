2ed1e52c2a572e744a77405ea7f0bb1627415e9a
erodrigufer <erodrigufer@gmail.com>
2022-03-28 16:38:40 +0200

    configure dotfiles

--- setupFirstRun.sh
+++ setupFirstRun.sh
@@ -3,6 +3,8 @@
 OS=''
 #########################################################################
 MAIN_USER="hap"
+# lf-26 is the Go terminal file manager
+PACKAGES2INSTALL="git vim curl go lf-26"
 
 #########################################################################
 # Colors for the text (they do not set the background color)
@@ -36,7 +38,7 @@ check_os(){
 		# otherwise all these characters must be escaped with the normal regex 
 		# grammar of grep
 		lsb_release -i | grep -i -E "(ubuntu|debian|kali)" || { print_error "System must be debian-based to run script."; exit -1 ; }
-		OS="linux"
+		OS="debian"
 		return
 	fi
 	if [ $(uname) = "FreeBSD" ]; then
@@ -44,6 +46,8 @@ check_os(){
 		return
 	fi
 
+	print_error "{$(uname)} System not supported!"	
+	exit -1
 }
 
 configure_users(){
@@ -66,24 +70,39 @@ setup_sshd(){
 # Install the bare minimum of necessary packages
 install_bare_packages(){
 	if [ ${OS} = "freebsd" ]; then
-		# lf-26 is the Go terminal file manager
-		local PACKAGES2INSTALL="git vim curl go lf-26"
-
+		
 		# Update the available remote repositories
 		pkg update	
 		# Install packages without further confirmation (--yes)
 		# For more information take a look at 'man pkg-install'
 		# To get more info about a particular package run:
 		# pkg search -R <name>
-		pkg install --yes ${PACKAGES2INSTALL} && print_info "${PACKAGES2INSTALL} were successfully installed!"
+		pkg install --yes ${PACKAGES2INSTALL} && print_info "${PACKAGES2INSTALL} were successfully installed!" || { print_error "Packages installation failed!"; exit -1; }
+		return
+	fi
+
+	if [ ${OS} = "debian" ]; then
+		return	
 	fi
 
 }
 
+configure_dotfiles(){
+	# change to home directory
+	cd ${HOME}
+	git clone https://github.com/erodrigufer/dotfiles.git
+	cd ./dotfiles
+	make
+	
+	# we do not need the zshrc file
+	rm -f ${HOME}/.zshrc
+	
+}
+
 main(){
 	check_os
-	echo $OS
 	install_bare_packages	
+	configure_dotfiles
 }
 
 main
