5e29c9e830e9e8defcf2090c07e82b129410f9b4
erodrigufer <erodrigufer@gmail.com>
2022-03-28 15:41:07 +0200

    check OS

--- setupFirstRun.sh
+++ setupFirstRun.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
+OS=''
 #########################################################################
 MAIN_USER="hap"
 
 #########################################################################
-
+# Colors for the text (they do not set the background color)
 COLOR_GREEN='\e[0;32m'
 NO_COLOR='\033[0m'
 COLOR_RED='\033[0;31m'
+COLOR_MAGENTA='\033[0;35m'
+COLOR_CYAN='\033[0;36m'
 
+#########################################################################
 
-# Properly log an error message to stdout
+# Print an info message to stdout. First parameter is string to be logged
+print_info(){
+	local INFO_COLOR=${COLOR_CYAN}
+	printf "[${INFO_COLOR}INFO${NO_COLOR}] $1\n"
+}
+
+# Print an error message to stdout. First parameter is string to be logged
 print_error(){
 	printf "[${COLOR_RED}ERROR${NO_COLOR}] $1\n"
 }
@@ -18,19 +28,25 @@ print_error(){
 # Check if the script is running in a valid OS (eventual support should be for
 # primary FreeBSD and Ubuntu)
 check_os(){
-# print Linux_Standard_Base release ID
-# use grep in egrep mode (-e), being 
-# case-insensitive (-i) and check for either 
-# ubuntu or debian
-echo "* Checking system distribution..."
-lsb_release -i | grep -i -E "(ubuntu|debian|kali)" || { print_error "System must be debian-based to run script."; exit -1 ; }
+	echo "* Checking system distribution..."
+	if [ uname = "Linux" ]; then
+		# print Linux_Standard_Base release ID use grep in egrep mode (-e), 
+		# being case-insensitive (-i) and check for either ubuntu or debian
+		# About egrep: with egrep it is easier to write the OR logic ( | ) 
+		# otherwise all these characters must be escaped with the normal regex 
+		# grammar of grep
+		lsb_release -i | grep -i -E "(ubuntu|debian|kali)" || { print_error "System must be debian-based to run script."; exit -1 ; }
+		OS="linux"
+	fi
+	if [ uname = "FreeBSD" ]; then
+		OS="freebsd"
+	fi
 
-# About egrep: with egrep it is easier to write the OR
-# logic ( | ) otherwise all these characters must be escaped
-# with the normal regex grammar of grep
 }
 
 configure_users(){
+	# Check if username already exists
+	
 	adduser ${MAIN_USER}
 	# give sudo rights to main user
 	adduser ${MAIN_USER} sudo	
@@ -45,8 +61,19 @@ setup_sshd(){
 	echo "Done"
 }
 
+# Install the bare minimum of necessary packages
+bare_packages(){
+	# Update the available remote repositories
+	pkg update	
+
+}
+
 main(){
 	check_os
+	echo $OS
+	
 }
 
 main
+
+# setup vim, setup lf, setup dotfiles (tmux, etc)
