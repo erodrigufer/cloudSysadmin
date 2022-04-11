ae5e9b3e9a364cc01a8612d8d061d86b2d96c6d0
erodrigufer <erodrigufer@gmail.com>
2022-04-11 13:43:59 +0200

    comment all data types as per Go comment guidelines

--- vultrClient/cmd/api/apiModels.go
+++ vultrClient/cmd/api/apiModels.go
@@ -5,31 +5,35 @@ import (
 	"time"
 )
 
-// Info to request the creation of a new instance
+// RequestCreateInstance has the info necessary to request the creation of a
+// new instance
 type RequestCreateInstance struct {
-	// omitempty= if value is not present, omit at encoding
-	ID       string `json:"id,omitempty"`
-	OS_id    int    `json:"os_id"`
-	Label    string `json:"label,omitempty"`
-	Hostname string `json:"hostname,omitempty"`
-	Region   string `json:"region"`
-	Plan     string `json:"plan"`
-	Backups  string `json:"backups"` // disabled (no backups)
+	ID       string   `json:"id,omitempty"` // omitempty= if value is not present, omit at encoding
+	OS_id    int      `json:"os_id"`
+	Label    string   `json:"label,omitempty"`
+	Hostname string   `json:"hostname,omitempty"`
+	Region   string   `json:"region"`
+	Plan     string   `json:"plan"`
+	Backups  string   `json:"backups"` // disabled (no backups)
+	SSHKeys  []string `json:"sshkey_id,omitempty"`
 	// with backups is more expensive
 }
 
-// Info received from response after creating instance
+// ResponseCreateInstance has the info received from response after requesting
+// to create an instance
 type ResponseCreateInstance struct {
 	Instance *CreatedInstance `json:"instance"`
 }
 
+// CreatedInstance is the data structure that describes a created instances
 type CreatedInstance struct {
-	ID               string    `json:"id"`
-	OS               string    `json:"os"`
-	RAM              int       `json:"ram"`
-	Disk             int       `json:"disk"`
-	MainIP           string    `json:"main_ip"`
-	VCPUCount        int       `json:"vcpu_count"`
+	ID        string `json:"id"`
+	OS        string `json:"os"`
+	RAM       int    `json:"ram"`
+	Disk      int    `json:"disk"`
+	MainIP    string `json:"main_ip"`
+	VCPUCount int    `json:"vcpu_count"`
+	// Region is the region where the instance is being deployed
 	Region           string    `json:"region"`
 	Plan             string    `json:"plan"`
 	CreationDate     time.Time `json:"date_created"`
@@ -55,13 +59,18 @@ type CreatedInstance struct {
 	DefaultPassword  string    `json:"default_password"`
 }
 
+// SSHKey is the data structure with the fields that describe a single SSH key
 type SSHKey struct {
 	ID           string    `json:"id"`
 	CreationDate time.Time `json:"date_created"`
-	Name         string    `json:"name"`
-	SSH_Pub_Key  string    `json:"ssh_key"`
+	// Name is the name given to a single SSH key
+	Name string `json:"name"`
+	// SSH_Pub_Key is the actual public ssh key
+	SSH_Pub_Key string `json:"ssh_key"`
 }
 
+// ResponseListSSHKeys has the information received after requesting a list with
+// the stored SSH keys in the system
 type ResponseListSSHKeys struct {
 	SSHKeys []SSHKey `json:"ssh_keys"` // slice of ssh keys
 	Meta    struct {
