eb2ee31a47937ae5344a8a09a220c5b918abeaee
erodrigufer <erodrigufer@gmail.com>
2022-05-05 15:22:34 +0200

    add more specific error message for 401 response

--- vultrClient/cmd/api/helpers.go
+++ vultrClient/cmd/api/helpers.go
@@ -16,11 +16,16 @@ func (app *application) addAuthToken(req *http.Request) {
 
 // checkResponseAPI, checks if the response from the API is correct, if not
 // it returns error.
-// The input parameter is the expected correct HTTP response.
+// The input parameter is the expected correct HTTP response (int) and the
+// http.Response being checked.
 func checkResponseAPI(resp *http.Response, correctResponse int) (err error) {
-	// if everything went well Vultr API responds with "correctResponse"
+	// if everything went well Vultr API responds with "correctResponse" (int)
 	if resp.StatusCode != correctResponse {
-		err = fmt.Errorf("request failed. Response status: %d, expected %d", resp.StatusCode, correctResponse)
+		if resp.StatusCode == 401 {
+			err = fmt.Errorf("request failed. Response status: %s, expected %d %s. A 401 error response normally takes place, because the IP from which the request was done is not green-listed to communicate with the Vultr API. Check under 'Account/API/Access Control' in the Vultr control panel, if you need to enable access to the API for further IPv4 and IPv6 addresses to perform your next request.", resp.Status, correctResponse, http.StatusText(correctResponse))
+			return
+		}
+		err = fmt.Errorf("request failed. Response status: %s, expected %d", resp.Status, correctResponse)
 		return
 	}
 	return nil
